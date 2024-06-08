f4ff08355ce343c36ee61ce470485ad231ddca2e
StavrosNik4 <stavniko@csd.auth.gr>
2024-06-08 19:22:36 +0300

    some new obstacles

--- src/ibug/MyEnv.java
+++ src/ibug/MyEnv.java
@@ -12,7 +12,7 @@ import javax.vecmath.Vector3f;
 public class MyEnv extends EnvironmentDescription {
     MyEnv(){
         // lights
-        light1SetPosition(0, 2, 0); // second parameter is height
+        light1SetPosition(5, 2, -4); // second parameter is height
         light1IsOn = true;
         light2IsOn = false;
         this.ambientLightColor = new Color3f(0,0,0);
@@ -22,14 +22,18 @@ public class MyEnv extends EnvironmentDescription {
         archColor = red; boxColor = darkgray; wallColor = blue;
 
         // obstacles
-        Wall w1 = new Wall(new Vector3d(-5, 0, 0), 10, 1, this);
-        w1.rotate90(1);
-        add(w1);
 
-//        bottle(this);
+//        addBoxObstacle(this);
+//
+//        addWallObstacle(this);
+//
+//        addPyramidObstacle(this);
+
+//        addMaze(this);
+
+        addCustomStructure(this);
 
         // robot
-//        add(new CherryAgent(new Vector3d(1,0,2),"cherry",0.1f));
         add(new MyRobot(new Vector3d(-8, 0, 0), "robot 1"));
 
 
@@ -42,6 +46,38 @@ public class MyEnv extends EnvironmentDescription {
 //        environment.add(new Box(new Vector3d(0,0,-0.5), new Vector3f(2,1,2),environment));
 //    }
 
-}
+    // Adds a big box obstacle
+    static void addBoxObstacle(EnvironmentDescription environment){
+        // Add a single box at the center
+        environment.add(new Box(new Vector3d(0, 0, 0), new Vector3f(4, 1, 4), environment));
+    }
 
-// 0.0617283950617284
+    // Adds a wall obstacle
+    static void addWallObstacle(EnvironmentDescription environment){
+        Wall w1 = new Wall(new Vector3d(-5, 0, 0), 10, 1, environment);
+        w1.rotate90(1);
+        environment.add(w1);
+    }
+
+    // Adds a custom structure using walls
+    static void addCustomStructure(EnvironmentDescription environment){
+
+        // Add the vertical wall
+        Wall verticalWall = new Wall(new Vector3d(-3, 0, 0), 10, 1, environment);
+        verticalWall.rotate90(1);
+        environment.add(verticalWall);
+
+        // Add the horizontal wall
+        Wall horizontalWall = new Wall(new Vector3d(-1, 0, -5), 5, 1, environment);
+        environment.add(horizontalWall);
+
+//        // Add the box
+//        Box box = new Box(new Vector3d(2, 0, 0), new Vector3f(3, 1, 3), environment);
+//        environment.add(box);
+
+
+    }
+
+
+
+}
